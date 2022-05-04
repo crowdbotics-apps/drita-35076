1d05470c8b54cae4720c57f53c9d1df637597767
Crowdbotics <team@crowdbotics.com>
2022-05-04 10:33:06 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/drita_35076/MainActivity.java
@@ -0,0 +1,15 @@
+package com.drita_35076;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "drita_35076";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "drita_35076",
+  url: "https://drita_35076.botics.co",
+  api: "https://drita_35076.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
